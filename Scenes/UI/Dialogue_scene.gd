7ed916256062e2eeb4ec19e745da1f3f4ea3eaf3
al406608 <al406608@uji.es>
2024-04-23 17:57:00 +0200

    Creacion ciudad y colliders
    Creacion ciudad y colliders de escenas, empiece de desarrollo del personaje

--- Scenes/UI/Dialogue_scene.gd
+++ Scenes/UI/Dialogue_scene.gd
@@ -1,6 +1,13 @@
 extends Control
 
+var text_shown = false
 
-func show_text(dialogue : String):
-	self.show()
-	$CenterContainer/Label.text = dialogue
+func show_text(dialogue : String, title : String = ""):
+	$TipLabel.hide()
+	$DialogueWindow.show()
+	$DialogueWindow/CenterContainer/Label.text = dialogue
+	$DialogueWindow/Title.text = title
+	text_shown = true
+
+func show_tip(item : String ):
+	$TipLabel/CenterContainer/HBoxContainer/Object.text = item
--- Scripts/Interactuable_component.gd
+++ Scripts/Interactuable_component.gd
@@ -1,15 +1,28 @@
 extends Node
 
 @export var dialogue : String = "Necesito texto"
+@export var title : String
 @onready var dialogue_prefab = preload("res://Scenes/UI/Dialogue_scene.tscn")
 var dialogue_prefab_instance 
+var in_reach = false
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Player"):
 		dialogue_prefab_instance = dialogue_prefab.instantiate()
 		add_child(dialogue_prefab_instance)
-		dialogue_prefab_instance.show_text(dialogue)
+		dialogue_prefab_instance.show_tip(title)
+		in_reach = true
 
 func _on_area_3d_body_exited(body):
 	if body.is_in_group("Player"):
 		if dialogue_prefab_instance != null :
 			dialogue_prefab_instance.queue_free()
+		in_reach = false
+
+func _unhandled_key_input(_event):
+	if Input.is_action_just_pressed("e_key_press") and in_reach:
+		if dialogue_prefab_instance != null:
+			if !dialogue_prefab_instance.text_shown:
+				dialogue_prefab_instance.show_text(dialogue,title)
+			else:
+				dialogue_prefab_instance.queue_free()
+		
