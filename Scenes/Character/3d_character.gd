1ffc47a1cb1d4592baf6f68b8dbbf535a25268e0
al406608 <al406608@uji.es>
2024-05-17 02:52:34 +0200

    Añadido de mirilla

--- Scenes/Character/3d_character.gd
+++ Scenes/Character/3d_character.gd
@@ -5,6 +5,7 @@ var speed
 const JUMP_VELOCITY = 4.5
 const WALK_SPEED = 3.0
 const SPRINT_SPEED = 6.0
+const RAY_LENGTH = 1.8
 #Variables con opcion a editar
 var sensitivity = 0.001
 var bob_freq = 2.0
@@ -15,9 +16,15 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
+var colliding : bool = false
+var crosshair = null
 #Funcion para ocultar el raton
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	var grupo = get_tree().get_nodes_in_group("controller")
+	if !grupo.is_empty():
+		crosshair = grupo[0].crosshair
+		crosshair.show()
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -26,6 +33,7 @@ func _unhandled_input(event):
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 
 func _physics_process(delta):
+	camera_raycast()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -60,3 +68,22 @@ func _headbob(time) -> Vector3 :
 	pos.y = sin(time * bob_freq) * bob_amp
 	pos.x = cos(time * bob_freq / 2 ) * bob_amp
 	return pos
+
+func camera_raycast():
+	var space_state = get_world_3d().direct_space_state
+	var cam = $Head/Camera3D
+	var mousepos = get_viewport().get_mouse_position()
+	var origin = cam.project_ray_origin(mousepos)
+	var end = origin + cam.project_ray_normal(mousepos) * RAY_LENGTH
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.exclude = [self]
+	query.collide_with_bodies = true
+	var result = space_state.intersect_ray(query)
+	if !result.is_empty() :
+		colliding = true
+		if crosshair != null:
+			crosshair.inspect_element(true)
+	else:
+		colliding = false
+		if crosshair != null:
+			crosshair.inspect_element(false)
---
+++ Scripts/crosshead.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+@onready var initial_color = self.modulate
+var inspection_color = Color.DARK_ORCHID
+var inspecting = false
+func _ready():
+	hide()
+
+func inspect_element(value):
+	if value == inspecting:
+		return
+	inspecting = value
+	if inspecting :
+		modulate = inspection_color
+	else:
+		modulate = initial_color
+	
